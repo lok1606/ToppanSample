3036f87e13bb8451c6367128d5ebc8c1a6e9bbc5
Cheung Lok Hang <lhcheung@lemc.com.hk>
2024-10-03 17:52:37 +0800

    aaaaaaaaaaaaaaa

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo /home/ec2-user/.local/bin/uvicorn --app-dir /Apps/fastapi app:app --host 0.0.0.0 --port 8000
+/home/ec2-user/.local/bin/uvicorn --app-dir /Apps/fastapi app:app --host 0.0.0.0 --port 8000
 
