32f3422e4e320f868a175073c380e174564659a3
Cheung Lok Hang <lhcheung@lemc.com.hk>
2024-10-08 17:03:30 +0800

    update test

--- test/test.py
+++ test/test.py
@@ -1,7 +1,20 @@
+import unittest
 from fastapi.testclient import TestClient
 from fastapiSample.app import app
 
-client = TestClient(app)
-def test_read_main():
-    response = client.get("/health")
-    assert response.status_code == 200
+class AppTests(unittest.TestCase):
+    client = TestClient(app)
+    def test_app(self):
+        response = client.get("/health")
+		self.assertEqual(response.status_code, 200)
+
+
+def suite():
+    _suite = unittest.TestSuite()
+    _suite.addTest(AppTests('test_app'))
+    return _suite
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
